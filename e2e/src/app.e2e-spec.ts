4a5b6ceabf47a0a1074739e06adf494171a281ca
Nsikan Essien <nsikane@live.co.uk>
2019-12-23 23:31:12 +0000

    Fix e2e test for landing page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Master math through challenge');
+    expect(page.getTitleText()).toEqual('Master maths through challenges.');
   });
 
   afterEach(async () => {
