9268b7ea72f858afe584c627cff088f64d5b1fa6
Nsikan Essien <nsikane@live.co.uk>
2019-12-24 02:40:44 +0000

    Add Auth Component to Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
+  { path: 'auth/signup', component: AuthComponent },
+  { path: 'auth/login', component: AuthComponent },
+  { path: 'auth', component: AuthComponent },
   { path: 'home', component: LandingPageComponent },
   { path: '', component: LandingPageComponent }
 ];
