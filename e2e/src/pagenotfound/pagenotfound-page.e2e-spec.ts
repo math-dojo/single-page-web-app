285796a62010d6c57f33db9a417fb82abf89de38
Nsikan Essien <nsikane@live.co.uk>
2019-12-26 23:12:29 +0000

    Add 404 page and e2e test for routing

---
+++ e2e/src/pagenotfound/pagenotfound-page.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { PageNotFoundPage } from './pagenotfound-page.po';
+import { browser, logging } from 'protractor';
+
+describe('PageNotFound Page', () => {
+  let page: PageNotFoundPage;
+
+  beforeEach(() => {
+    page = new PageNotFoundPage();
+  });
+
+  it('should display page not found text', () => {
+    page.navigateToPath('/i-cant-possibly-exist');
+    expect(page.getPageNotFoundMainText()).toMatch('The Page You Are Looking For Cannot Be Found');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
