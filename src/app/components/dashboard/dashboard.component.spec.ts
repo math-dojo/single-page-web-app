0bd6d8694e7642bc78a5fa1a83315189173dde8d
Nsikan Essien <nsikane@live.co.uk>
2020-02-09 12:00:53 +0000

    Add clarity imports to fix failing tests

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ClarityModule } from '@clr/angular';
 
 import { DashboardComponent } from './dashboard.component';
@@ -10,7 +11,7 @@ describe('DashboardComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
-      imports: [ClarityModule]
+      imports: [ClarityModule, RouterTestingModule]
     })
     .compileComponents();
   }));
