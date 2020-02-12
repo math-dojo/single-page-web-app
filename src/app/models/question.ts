3a600edc8853e5568762e5dc459ccd62993a378d
Nsikan Essien <nsikane@live.co.uk>
2020-02-12 23:15:56 +0000

    Refactor question into UI object and dto

--- src/app/models/question.ts
+++ src/app/models/question.ts
@@ -1,36 +1,46 @@
-export class Question {
-    public readonly title: string;
-    public readonly body: string;
-    public readonly sampleAnswer: string;
-    public readonly hints: string[];
-    public readonly successRate: number;
-    public readonly difficulty: string;
-    public solved: boolean;
+import { QuestionDto } from './question-dto';
 
-    public constructor({
-        title,
-        body,
-        sampleAnswer,
-        hints,
-        solved,
-        successRate,
-        difficulty
-    }: {
-        title: string,
-        body: string,
-        sampleAnswer: string,
-        hints: string[],
-        solved: boolean,
-        successRate: number,
-        difficulty: string
-    }) {
-        this.title = title;
-        this.body = body;
-        this.sampleAnswer = sampleAnswer;
-        this.hints = hints;
-        this.solved = solved;
-        this.successRate = successRate;
-        this.difficulty = difficulty;
-    }
+export class Question extends QuestionDto {
+  public readonly formattedTitle: string;
+  public readonly formattedSuccessRate: string;
+  public readonly formattedDifficulty: string;
 
+  public constructor({
+    title,
+    body,
+    sampleAnswer,
+    hints,
+    solved,
+    successRate,
+    difficulty
+  }: {
+    title: string,
+    body: string,
+    sampleAnswer: string,
+    hints: string[],
+    solved: boolean,
+    successRate: number,
+    difficulty: string
+  }) {
+    super({
+      title,
+      body,
+      sampleAnswer,
+      hints,
+      solved,
+      successRate,
+      difficulty
+    });
+
+    this.formattedTitle = this.title
+      .replace((/(^|-)(\w)/g), (match, p1, p2, offset, string) => {
+        if (offset > 0) {
+          return (' ' + p2.toUpperCase());
+        }
+        return p2.toUpperCase();
+    });
+
+    this.formattedSuccessRate = (this.successRate * 100).toFixed();
+    this.formattedDifficulty = (this.difficulty.replace(/^\w/, (match) => match.toUpperCase()));
+  }
 }
