72a275b3705af5b4204d1417b3e8b38c2035ffb5
Nsikan Essien <nsikane@live.co.uk>
2020-03-15 05:23:12 +0000

    Create question service stub

---
+++ src/testing/question.service.stub.ts
@@ -0,0 +1,19 @@
+import { of } from 'rxjs';
+import { TopicDto } from '../app/models/topic-dto';
+import { QuestionDto } from '../app/models/question-dto';
+import { QuestionService } from '../app/services/question.service';
+
+export const QuestionServiceStub: Partial<QuestionService> = {
+    getQuestionsForTopic: jasmine.createSpy('getQuestionsForTopic')
+            .and.returnValue(of([QuestionDto.createDtoWithNonEmptyFields()])),
+
+    getTopics: jasmine.createSpy('getQuestionsForTopic')
+        .and.returnValue(of([TopicDto.createDtoWithNonEmptyFields()])),
+
+    getTopicWithTitle: jasmine.createSpy('getQuestionsForTopic')
+        .and.returnValue(of([TopicDto.createDtoWithNonEmptyFields()])),
+
+    getQuestionWithTitle: jasmine.createSpy('getQuestionsForTopic')
+    .and.returnValue(of(QuestionDto.createDtoWithNonEmptyFields()))
+    
+};
