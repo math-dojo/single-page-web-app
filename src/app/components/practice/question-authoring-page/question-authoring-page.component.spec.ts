9ed49edc89956ab7ed2dd641357e568fd6f7d309
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-07-14 05:38:08 +0100

    Put parameterised input unit tests in sep describe

--- src/app/components/practice/question-authoring-page/question-authoring-page.component.spec.ts
+++ src/app/components/practice/question-authoring-page/question-authoring-page.component.spec.ts
@@ -59,67 +59,70 @@ describe('QuestionAuthoringPageComponent', () => {
     expect(component.onSubmit).toHaveBeenCalledTimes(1);
   });
 
-  const parameters = [
-    {
-      description: 'title input control when text is entered', controlName: 'title',
-      cssSelector: '#mtdj__question-auth-input-title input', inputValue: 'something-really-hard'
-    },
-    {
-      description: 'topic input control when text is entered', controlName: 'parentTopicTitle',
-      cssSelector: '#mtdj__question-auth-input-topic input', inputValue: 'something-really-hard'
-    },
-    {
-      description: 'body input control when text is entered', controlName: 'body',
-      cssSelector: '#mtdj__question-auth-input-body textarea', inputValue: 'some quqestion'
-    },
-    {
-      description: 'sampleAnswer input control when text is entered', controlName: 'sampleAnswer',
-      cssSelector: '#mtdj__question-auth-input-sample_answer textarea', inputValue: 'some sample answer'
-    },
-    {
-      description: 'answer input control when text is entered', controlName: 'answer',
-      cssSelector: '#mtdj__question-auth-input-answer textarea', inputValue: 'some correct answer'
-    },
-    {
-      description: 'hint1 input control when text is entered', controlName: 'hint1',
-      cssSelector: '#mtdj__question-auth-input-hint1 input', inputValue: 'some hint'
-    },
-    {
-      description: 'hint2 input control when text is entered', controlName: 'hint2',
-      cssSelector: '#mtdj__question-auth-input-hint2 input', inputValue: 'some hint'
-    },
-    {
-      description: 'hint3 input control when text is entered', controlName: 'hint3',
-      cssSelector: '#mtdj__question-auth-input-hint3 input', inputValue: 'some hint'
-    },
-    {
-      description: 'option1 input control when text is entered', controlName: 'option1',
-      cssSelector: '#mtdj__question-auth-input-option1 input', inputValue: 'some option'
-    },
-    {
-      description: 'option2 input control when text is entered', controlName: 'option2',
-      cssSelector: '#mtdj__question-auth-input-option2 input', inputValue: 'some option'
-    },
-    {
-      description: 'option3 input control when text is entered', controlName: 'option3',
-      cssSelector: '#mtdj__question-auth-input-option3 input', inputValue: 'some option'
-    },
-    {
-      description: 'option4 input control when text is entered', controlName: 'option4',
-      cssSelector: '#mtdj__question-auth-input-option4 input', inputValue: 'some option'
-    }
-  ];
-  parameters.forEach(({ description, cssSelector, inputValue, controlName }) => {
-    it(`should update the value of the ${description}`, () => {
-      const inputFormElement = fixture.debugElement.query(By.css(cssSelector));
-      const inputQuestionTitle = inputValue;
+  describe('Input controls', () => {
+    const parameters = [
+      {
+        description: 'title input control when text is entered', controlName: 'title',
+        cssSelector: '#mtdj__question-auth-input-title input', inputValue: 'something-really-hard'
+      },
+      {
+        description: 'topic input control when text is entered', controlName: 'parentTopicTitle',
+        cssSelector: '#mtdj__question-auth-input-topic input', inputValue: 'something-really-hard'
+      },
+      {
+        description: 'body input control when text is entered', controlName: 'body',
+        cssSelector: '#mtdj__question-auth-input-body textarea', inputValue: 'some quqestion'
+      },
+      {
+        description: 'sampleAnswer input control when text is entered', controlName: 'sampleAnswer',
+        cssSelector: '#mtdj__question-auth-input-sample_answer textarea', inputValue: 'some sample answer'
+      },
+      {
+        description: 'answer input control when text is entered', controlName: 'answer',
+        cssSelector: '#mtdj__question-auth-input-answer textarea', inputValue: 'some correct answer'
+      },
+      {
+        description: 'hint1 input control when text is entered', controlName: 'hint1',
+        cssSelector: '#mtdj__question-auth-input-hint1 input', inputValue: 'some hint'
+      },
+      {
+        description: 'hint2 input control when text is entered', controlName: 'hint2',
+        cssSelector: '#mtdj__question-auth-input-hint2 input', inputValue: 'some hint'
+      },
+      {
+        description: 'hint3 input control when text is entered', controlName: 'hint3',
+        cssSelector: '#mtdj__question-auth-input-hint3 input', inputValue: 'some hint'
+      },
+      {
+        description: 'option1 input control when text is entered', controlName: 'option1',
+        cssSelector: '#mtdj__question-auth-input-option1 input', inputValue: 'some option'
+      },
+      {
+        description: 'option2 input control when text is entered', controlName: 'option2',
+        cssSelector: '#mtdj__question-auth-input-option2 input', inputValue: 'some option'
+      },
+      {
+        description: 'option3 input control when text is entered', controlName: 'option3',
+        cssSelector: '#mtdj__question-auth-input-option3 input', inputValue: 'some option'
+      },
+      {
+        description: 'option4 input control when text is entered', controlName: 'option4',
+        cssSelector: '#mtdj__question-auth-input-option4 input', inputValue: 'some option'
+      }
+    ];
+    parameters.forEach(({ description, cssSelector, inputValue, controlName }) => {
+      it(`should update the value of the ${description}`, () => {
+        const inputFormElement = fixture.debugElement.query(By.css(cssSelector));
+        const inputQuestionTitle = inputValue;
 
-      inputFormElement.nativeElement.value = inputQuestionTitle;
-      inputFormElement.nativeElement.dispatchEvent(new InputEvent('input'));
+        inputFormElement.nativeElement.value = inputQuestionTitle;
+        inputFormElement.nativeElement.dispatchEvent(new InputEvent('input'));
 
-      expect(fixture.componentInstance.newQuestionForm.controls[controlName].value).toEqual(inputQuestionTitle);
+        expect(fixture.componentInstance.newQuestionForm.controls[controlName].value).toEqual(inputQuestionTitle);
+      });
     });
   });
+
 
   //TODO: Write sepearate test case for select option: https://stackoverflow.com/questions/5678210/select-dropdown-menu-option-with-javascript
 
