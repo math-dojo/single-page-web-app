b03ed45530b130850b184c57a4cedb802d8c2790
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-07-17 06:42:43 +0100

    Document generic error as a type

--- src/app/utilities/generic-error.interface.ts
+++
@@ -1,5 +0,0 @@
-/**
- * A generic error interface used for performing assertions on types
- * of errors thrown from services.
- */
-export type GenericErrorInterface<T> = new (message?: string) => T;
