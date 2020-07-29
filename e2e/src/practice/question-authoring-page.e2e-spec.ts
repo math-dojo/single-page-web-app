cbb1637d869a14d29a0e833243ace81ce6e10257
noce2 <20339399+noce2@users.noreply.github.com>
2020-07-29 22:01:55 +0100

    Fix failing e2e test for question submission

--- e2e/src/practice/question-authoring-page.e2e-spec.ts
+++ e2e/src/practice/question-authoring-page.e2e-spec.ts
@@ -9,7 +9,7 @@ describe('Given I navigate to the Question Authoring Page', () => {
     page.navigateToFeatureRoot();
   });
 
-  it('it should display a success alert and reset the form when I submit a valid form', () => {
+  it('it should display a success alert and reset the form when I submit a valid form', async () => {
     expect(page.submissionErrorAlert.isPresent()).toBe(false, 'a page submission error alert could be seen on startup');
     expect(page.submissionSuccessAlert.isPresent()).toBe(false, 'a page submission success alert could be seen on startup');
 
@@ -20,6 +20,21 @@ describe('Given I navigate to the Question Authoring Page', () => {
     expect(page.submissionErrorAlert.isPresent()).toBe(false, 'a page submission error alert could be seen on submit');
     expect(page.submissionSuccessAlert.isPresent()).toBe(true, 'no submission success alert could be seen on submit');
     expect(page.checkFormEmpty()).toBe(true, 'form was not empty');
+
+    /*
+     * The nature of this test means that a 404 will be returned from the backend.
+     * As default browser behaviour is to log this as a SEVERE error, this will
+     * cause the test to fail in the afterEach performed after every test.
+     * The following block removes the expected errors and performs the validaton
+     * for no other SEVERE errors, given the buffer can only be read once.
+     */
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    const sanitisedLogs: logging.Entry[] = logs.filter((each) => 
+      ! (/(.*)\/questions\/(.*) - Failed to load resource/.test(each.message)));
+    expect(sanitisedLogs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+
   });
 
   afterEach(async () => {
