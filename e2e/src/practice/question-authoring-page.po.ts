e0ea508155ad656597d16aa17eff4ac891514fe7
noce2 <20339399+noce2@users.noreply.github.com>
2020-07-29 06:16:41 +0100

    Use element instead of element.all in authoring PO

--- e2e/src/practice/question-authoring-page.po.ts
+++ e2e/src/practice/question-authoring-page.po.ts
@@ -4,51 +4,63 @@ import { Utils } from '../utils/utils.po';
 export class QuestionAuthoringPage extends Utils {
 
   get form() {
-    return element.all(by.css('.mtdj__question-auth-input-container form'));
+    return element(by.css('.mtdj__question-auth-input-container form'));
   }
 
   get titleInput() {
-    return element.all(by.css('#mtdj__question-auth-input-title input'));
+    return element(by.css('#mtdj__question-auth-input-title input'));
   }
 
   get topicInput() {
-    return element.all(by.css('#mtdj__question-auth-input-topic input'));
+    return element(by.css('#mtdj__question-auth-input-topic input'));
   }
 
   get sampleAnswerInput() {
-    return element.all(by.css('#mtdj__question-auth-input-sample_answer textarea'));
+    return element(by.css('#mtdj__question-auth-input-sample_answer textarea'));
   }
 
   get answerInput() {
-    return element.all(by.css('#mtdj__question-auth-input-answer textarea'));
+    return element(by.css('#mtdj__question-auth-input-answer textarea'));
   }
 
   get hint1Input() {
-    return element.all(by.css('#mtdj__question-auth-input-hint1 input'));
+    return element(by.css('#mtdj__question-auth-input-hint1 input'));
   }
 
   get hint2Input() {
-    return element.all(by.css('#mtdj__question-auth-input-hint2 input'));
+    return element(by.css('#mtdj__question-auth-input-hint2 input'));
   }
 
   get hint3Input() {
-    return element.all(by.css('#mtdj__question-auth-input-hint3 input'));
+    return element(by.css('#mtdj__question-auth-input-hint3 input'));
   }
 
   get option1Input() {
-    return element.all(by.css('#mtdj__question-auth-input-option1 input'));
+    return element(by.css('#mtdj__question-auth-input-option1 input'));
   }
 
   get option2Input() {
-    return element.all(by.css('#mtdj__question-auth-input-option2 input'));
+    return element(by.css('#mtdj__question-auth-input-option2 input'));
   }
 
   get option3Input() {
-    return element.all(by.css('#mtdj__question-auth-input-option3 input'));
+    return element(by.css('#mtdj__question-auth-input-option3 input'));
   }
 
   get option4Input() {
-    return element.all(by.css('#mtdj__question-auth-input-option4 input'));
+    return element(by.css('#mtdj__question-auth-input-option4 input'));
+  }
+
+  get submissionSuccessAlert() {
+    return element(by.css('.mtdj__question-auth-input-container .alert.alert-success'));
+  }
+
+  get submissionErrorAlert() {
+    return element(by.css('.mtdj__question-auth-input-container .alert.alert-danger'));
+  }
+
+  get submitButton() {
+    return element(by.css('btn btn-primary'));
   }
 
   navigateToFeatureRoot() {
