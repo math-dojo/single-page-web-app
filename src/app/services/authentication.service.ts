2980211b09ebce5ee83eddb5f43396be749c3257
noce2 <20339399+noce2@users.noreply.github.com>
2020-08-30 19:52:26 +0100

    Add login method to authenication svc

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
+import { UserPermission } from '../models/permissions';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../models/user';
+import { AuthenticationServiceError } from './authentication-service.error';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private readonly expectedCreds = new Set(Object.keys(UserPermission).map(each => each.toLowerCase()));
+
+  private authenticatedUserSubject: BehaviorSubject<User>;
+
   signupNewUser(newUser: { name: string; email: string; password: string; }) {
     throw new Error('Method not implemented.');
   }
 
   constructor() { }
+
+  login(username: string, password: string): Observable<User> {
+    if ((username === password) && this.expectedCreds.has(username)) {
+      this.authenticatedUserSubject = new BehaviorSubject(
+        new User({
+          name: username,
+          permissions: new Set([UserPermission[username.toUpperCase()] as UserPermission]),
+          belongsToOrgWithId: 'default'
+        })
+      );
+      return this.authenticatedUserSubject.asObservable();
+    } else {
+      throw new AuthenticationServiceError(`the username, ${username}, or its supplied credentials are invalid`);
+    }
+  }
+
 }
