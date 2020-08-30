ae45c6b9d900d162927fd137d3e7410f38cb1e8a
noce2 <20339399+noce2@users.noreply.github.com>
2020-08-30 19:53:29 +0100

    test successful login returns a user observable

--- src/app/services/authentication.service.spec.ts
+++ src/app/services/authentication.service.spec.ts
@@ -1,14 +1,42 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { UserPermission } from '../models/permissions';
 
 describe('AuthenticationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let authService: AuthenticationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    authService = TestBed.inject(AuthenticationService);
+  });
 
   it('should be created', () => {
     const service: AuthenticationService = TestBed.inject(AuthenticationService);
     expect(service).toBeTruthy();
   });
+
+  it('should return an observable user when login is successful', async(() => {
+    const username = 'consumer';
+    const password = username;
+    const returnedUser: Observable<User> = authService.login(username, password);
+    const expectedPermission = UserPermission.CONSUMER;
+    returnedUser.subscribe({
+      next: (value) => {
+        expect(value.name).toEqual(username);
+        expect(value.permissions.size).toEqual(1);
+        expect(value.permissions.has(expectedPermission))
+          .withContext( `the permission contained, ${JSON.stringify(
+            Array.from(value.permissions.values()))}, did not matched the expected, ${expectedPermission} `)
+          .toEqual(true);
+      },
+      error: (error) => {
+        fail(`the returned user did not match the expectation because: ${error.message}`);
+      }
+    });
+  }));
 
   afterEach(() => {
     TestBed.resetTestingModule();
