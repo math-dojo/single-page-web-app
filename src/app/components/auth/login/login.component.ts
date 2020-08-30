84616d4211c5ba3d8e9f462520ac5c652ebc93bd
noce2 <20339399+noce2@users.noreply.github.com>
2020-08-30 15:13:14 +0100

    Add login html, css and component from signup

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  userLoginForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  ngOnInit(): void {
+  constructor(private authentiationService: AuthenticationService) {
+
+   }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.userLoginForm.value);
+    // this.signupNewUser(this.newUserSignupForm.value);
+  }
+
+
+  private signupNewUser(newUser) {
+    this.authentiationService.signupNewUser(newUser);
   }
 
 }
