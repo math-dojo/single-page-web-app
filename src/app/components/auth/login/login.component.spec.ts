c1844667ce675dbdb1c335ff681c8aa7aec51c47
noce2 <20339399+noce2@users.noreply.github.com>
2020-08-31 08:30:38 +0100

    Provide authentication service for login component

--- src/app/components/auth/login/login.component.spec.ts
+++ src/app/components/auth/login/login.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { SinonStubbedInstance, createStubInstance } from 'sinon';
 
 import { LoginComponent } from './login.component';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authSericeSpy: SinonStubbedInstance<AuthenticationService>;
 
   beforeEach(async(() => {
+    authSericeSpy = createStubInstance(AuthenticationService);
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      providers: [
+        { provide: AuthenticationService, useValue: authSericeSpy }
+      ]
     })
     .compileComponents();
   }));
