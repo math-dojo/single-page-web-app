d55d64ff51d3d80f2bdd61d6bfbf33bf14b72b9d
noce2 <20339399+noce2@users.noreply.github.com>
2020-09-29 22:25:58 +0100

    Linting on validator files

--- src/app/components/practice/question-authoring-page/question-title.validator.ts
+++ src/app/components/practice/question-authoring-page/question-title.validator.ts
@@ -1,42 +1,55 @@
 import { Injectable } from '@angular/core';
-import { AsyncValidator, AbstractControl, ValidationErrors } from '@angular/forms';
+import {
+  AsyncValidator,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { map, catchError, shareReplay } from 'rxjs/operators';
 import { QuestionService } from 'src/app/services/question.service';
 
 @Injectable({ providedIn: 'root' })
 export class QuestionTitleValidator implements AsyncValidator {
-  constructor(private questionService: QuestionService) { }
+  constructor(private questionService: QuestionService) {}
 
   validate(
     ctrl: AbstractControl
   ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
     const titleToValidate = ctrl.value as string;
-    return this.questionService.searchForQuestionBy({title: titleToValidate}).pipe(
-      shareReplay(1),
-      map((questionServiceResponse) => {
-        if (questionServiceResponse
-          && (questionServiceResponse.questions.length > 0)
-          && (
-            questionServiceResponse.questions.filter(each => each.title === titleToValidate).length >= 1
-            )) {
-          return this.createQuestionTitleValidationError(`a question with title "${titleToValidate}" already exists`);
-        }
-        return null;
-      }),
-      catchError((error) => {
-        // log this error appropriately
-        return of(this.createQuestionTitleValidationError(
-          `the question title "${ctrl.value}" could not be verified at this time, please try again later`));
-      })
-    );
+    return this.questionService
+      .searchForQuestionBy({ title: titleToValidate })
+      .pipe(
+        shareReplay(1),
+        map((questionServiceResponse) => {
+          if (
+            questionServiceResponse &&
+            questionServiceResponse.questions.length > 0 &&
+            questionServiceResponse.questions.filter(
+              (each) => each.title === titleToValidate
+            ).length >= 1
+          ) {
+            return this.createQuestionTitleValidationError(
+              `a question with title "${titleToValidate}" already exists`
+            );
+          }
+          return null;
+        }),
+        catchError((error) => {
+          // log this error appropriately
+          return of(
+            this.createQuestionTitleValidationError(
+              `the question title "${ctrl.value}" could not be verified at this time, please try again later`
+            )
+          );
+        })
+      );
   }
 
   private createQuestionTitleValidationError(errorMessage: string) {
     return {
       titleAlreadyExists: {
-        errorMessage
-      }
+        errorMessage,
+      },
     };
   }
 }
