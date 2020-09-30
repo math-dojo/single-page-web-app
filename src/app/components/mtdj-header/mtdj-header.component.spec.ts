43ae092487bbd404fbb89784dd6c756715121d04
noce2 <20339399+noce2@users.noreply.github.com>
2020-09-30 07:28:37 +0100

    Add host component to allow header unit tests

--- src/app/components/mtdj-header/mtdj-header.component.spec.ts
+++ src/app/components/mtdj-header/mtdj-header.component.spec.ts
@@ -1,22 +1,24 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
 
 import { MtdjHeaderComponent } from './mtdj-header.component';
 
 describe('MtdjHeaderComponent', () => {
-  let component: MtdjHeaderComponent;
-  let fixture: ComponentFixture<MtdjHeaderComponent>;
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MtdjHeaderComponent ],
+      declarations: [ MtdjHeaderComponent, TestHostComponent ],
       imports: [ClarityModule]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MtdjHeaderComponent);
+    fixture = TestBed.createComponent(TestHostComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -26,11 +28,25 @@ describe('MtdjHeaderComponent', () => {
   // inside a clr-main-container component. In the app the component meets this
   // condition but in its unit tests it wouldn't.
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+    const header = fixture.debugElement.query(
+      By.css('.header')
+    );
+    expect(header).toBeTruthy();
   });
 
   afterEach(() => {
     TestBed.resetTestingModule();
   });
 });
+
+@Component({
+  template: `
+  <clr-main-container class="main-container">
+    <app-mtdj-header></app-mtdj-header>
+  </clr-main-container>`
+})
+class TestHostComponent {
+
+}
